7e203fcc851d637ff711c4aa3ba73412e135ad78
giogio798 <giorgio.c.taliento@gmail.com>
2020-12-30 18:38:10 +0100

    Set-up creazioni Gruppi

--- app/src/main/java/it/gadg/contagiapp/CreaGruppoActivity.java
+++ app/src/main/java/it/gadg/contagiapp/CreaGruppoActivity.java
@@ -3,12 +3,40 @@ package it.gadg.contagiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreaGruppoActivity extends AppCompatActivity {
 
+    EditText nGruppo;
+    FirebaseFirestore db;
+
+    public void CGruppoButton(View view) {
+
+        nGruppo = findViewById(R.id.nGruppo);
+        String NomeGruppo = nGruppo.getText().toString();
+        Gruppo g = new Gruppo(NomeGruppo);
+        db.collection("Gruppi")
+                .add(g);
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_gruppo);
     }
+
+
 }
+
+
+
