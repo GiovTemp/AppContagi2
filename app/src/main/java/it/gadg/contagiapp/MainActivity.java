14888d5a92ff94ae9fddad3fb763689368562528
Giovanni <giovannitempestainfo@gmail.com>
2021-01-10 16:31:25 +0100

    setup controllo etichetta main

--- app/src/main/java/it/gadg/contagiapp/MainActivity.java
+++ app/src/main/java/it/gadg/contagiapp/MainActivity.java
@@ -6,7 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
 import it.gadg.contagiapp.evento.CreaEventoActivity;
 import it.gadg.contagiapp.evento.InviaPartecipazioneEvento;
@@ -21,12 +30,28 @@ public class MainActivity extends Activity {
 
     private FirebaseAuth mAuth; //dichiaro variabile per l'auenticazione firebase
 
+    FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth= FirebaseAuth.getInstance();
+        FirebaseUser u = mAuth.getCurrentUser();
+        String id = u.getUid();
+        db = FirebaseFirestore.getInstance();
 
+        db.collection("Utenti")
+                .document(id)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        String etichetta = (String) document.get("etichetta");
+                    }
+                });
     }
 
     public void logout(View view) {
